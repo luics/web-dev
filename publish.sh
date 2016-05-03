b0694bb8f4b39814027b646c6a7ee9dc3c52af97
luics <luics.xu@gmail.com>
2016-05-03 22:41:39 +0800

    update book

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-if ! [ _book/.git ]; then
+if ! [ -e _book/.git ]; then
   rm -rf _book
   git clone git@github.com:luics/web-dev.git _book
-  git fetch gh-pages
-  git checkout gh-pages
+  cd _book
+  git checkout -b gh-pages --track origin/gh-pages
+  cd ..
 fi
 
 gitbook build
