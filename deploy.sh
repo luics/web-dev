0bc46026ee0c4a2e32c721a41beae9731ae3647a
luics <luics.xu@gmail.com>
2016-05-05 19:18:54 +0800

    use new deploy key

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,6 @@ function doCompile {
   npm install gitbook-cli -g
   gitbook install
   gitbook build . ./out
-  cp deploy_key ./out
-  cp deploy_key.enc ./out
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -54,14 +52,11 @@ git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-#ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-#ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-#openssl aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_237e6e9123a6_key -iv $encrypted_237e6e9123a6_iv -in ../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
+rm -f deploy_key
 
 # Now that we're all set up, we can push.
 git push ${SSH_REPO} ${TARGET_BRANCH}
