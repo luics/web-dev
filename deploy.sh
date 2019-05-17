56045bb835c4c79a875d029176104f38ba90a6f0
luics <luics.xu@gmail.com>
2019-05-17 17:24:38 +0800

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  npm install -g gitbook-cli@2.1.2
+  npm install -g gitbook-cli
   gitbook install
   gitbook build . ./out
   cp -rf examples ./out/
